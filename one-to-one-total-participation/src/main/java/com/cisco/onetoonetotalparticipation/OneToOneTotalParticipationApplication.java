304d511951d9902db4d9e7705135ec1c501927b6
sovisrushain <sovisrushain@gmail.com>
2023-01-21 22:42:46 +0530

    one to one total participation

---
+++ one-to-one-total-participation/src/main/java/com/cisco/onetoonetotalparticipation/OneToOneTotalParticipationApplication.java
@@ -0,0 +1,13 @@
+package com.cisco.onetoonetotalparticipation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToOneTotalParticipationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneToOneTotalParticipationApplication.class, args);
+	}
+
+}
